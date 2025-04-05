fed6cf1a61c5ea43c74a9c970a3ba277b7d3564b
Luis Daniel Aponte Daza <ldaponte98@gmail.com>
2025-04-05 13:14:05 -0500

    Ajuste telefono

--- resources/views/event/external/event-external-script.blade.php
+++ resources/views/event/external/event-external-script.blade.php
@@ -21,9 +21,17 @@
         if ($('select[name="birthdayDay"]').val() == "" || $('select[name="birthdayDay"]').val() == null) { showAlert("Error", "El dia de nacimiento es obligatorio", "error"); return; }
         if ($('select[name="birthdayMonth"]').val() == "" || $('select[name="birthdayMonth"]').val() == null) { showAlert("Error", "El mes de nacimiento es obligatorio", "error"); return; }
         if ($('select[name="birthdayYear"]').val() == "" || $('select[name="birthdayYear"]').val() == null) { showAlert("Error", "El año de nacimiento es obligatorio", "error"); return; }
+        if (!validatePhone($('input[name="phone"]').val().trim())) { showAlert("Error", "El número de telefono no es valido", "error"); return; }
         setLoadingFullScreen(true);
         setTimeout(() => {
             $("#form").submit()
         }, 500);
+    }
+
+    function validatePhone(phone) {
+        if (phone[0] != 3) return false;
+        if (phone.trim().length != 10) return false;
+        const regexExp = /^(?:\+?57)?(?:\(?(\d{1,3})\)?[\s\-]?)?(\d{3})[\s\-]?(\d{4})$/;
+        return regexExp.test(phone);
     }
 </script>
